58f34f2531f5e3e38fa4489761dd09691f651020
padoma <psdonosom@gmail.com>
2026-07-18 23:46:13 -0400

    ACTUALIZAR

--- productos.js
+++ productos.js
@@ -3546,6 +3546,37 @@ const productos = {
         "tipo": "simple",
         "unitario": 900,
         "mayor": 900
+    },
+    "co150": {
+        "codigo": "CO150",
+        "nombre": "CO150",
+        "imagen": "img/co150.jpg",
+        "tipo": "simple",
+        "unitario": 2000,
+        "mayor": 1800
+    },
+    "co151": {
+        "codigo": "CO151",
+        "nombre": "CO151",
+        "imagen": "img/co151.jpg",
+        "tipo": "simple",
+        "unitario": 2000,
+        "mayor": 1500
+    },
+    "co153": {
+        "codigo": "CO153",
+        "nombre": "CO153",
+        "imagen": "img/co153.jpg",
+        "tipo": "simple",
+        "unitario": 52,
+        "mayor": 1
+    },
+    "co154": {
+        "codigo": "CO154",
+        "nombre": "CO154",
+        "imagen": "img/co154.jpg",
+        "tipo": "simple",
+        "unitario": 56,
+        "mayor": 2
     }
-
 };
